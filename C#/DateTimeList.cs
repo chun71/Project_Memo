6331377f111869914bf87ddb8571ec4bbc19896a
李家準 <leechiachun71@gmail.com>
2024-08-05 16:00:34 +0800

    2024.08.05_更新
    增加預設時間判定

--- C#/DateTimeList.cs
+++ C#/DateTimeList.cs
@@ -1,11 +1,12 @@
 ﻿
+using PIRM.Repository.IRiskMonDW.Core.EnumType;
+
 namespace PIRM.Business.Utility.Common
 {
-    /// <summary>
-    /// DateTimeList
-    /// </summary>
     public sealed class DateTimeList
     {
+        private DateTimeList() { }
+
         /// <summary>
         /// 判斷頻率取得 DateTime 清單
         /// </summary>
@@ -17,24 +18,100 @@ namespace PIRM.Business.Utility.Common
         {
             List<DateTime> dateTimeList = new List<DateTime>();
 
-            if (dataFrequency == Repository.Utility.EnumType.DataFrequency.MONTHLY)
+            List<DateTime> dateTimeRange = DateTimeRangeInit();
+
+            if (dataFrequency == DataFrequency.MONTHLY)
             {
+                minTime = minTime.HasValue ? minTime.Value : dateTimeRange[0];
+                maxTime = maxTime.HasValue ? maxTime.Value : dateTimeRange[1];
+
                 dateTimeList = GetDateTimeListMonthly(minTime.Value, maxTime.Value);
             }
-            else if (dataFrequency == Repository.Utility.EnumType.DataFrequency.QUARTERLY)
+            else if (dataFrequency == DataFrequency.QUARTERLY)
             {
+                minTime = minTime.HasValue ? minTime.Value : dateTimeRange[2];
+                maxTime = maxTime.HasValue ? maxTime.Value : dateTimeRange[3];
+
                 dateTimeList = GetDateTimeListQuarterly(minTime.Value, maxTime.Value);
             }
-            else if (dataFrequency == Repository.Utility.EnumType.DataFrequency.SEMI_ANNUAL)
+            else if (dataFrequency == DataFrequency.SEMI_ANNUAL)
             {
                 dateTimeList = GetDateTimeListSemiAnnual(minTime.Value, maxTime.Value);
             }
-            else if (dataFrequency == Repository.Utility.EnumType.DataFrequency.ANNUAL)
+            else if (dataFrequency == DataFrequency.ANNUAL)
             {
                 dateTimeList = GetDateTimeListAnnual(minTime.Value, maxTime.Value);
             }
 
             return dateTimeList;
+        }
+
+        /// <summary>
+        /// 預先載入日期範圍
+        /// </summary>
+        /// <returns></returns>
+        private static List<DateTime> DateTimeRangeInit()
+        {
+            DateTime? minDateMonthly = null;
+            DateTime? maxDateMonthly = null;
+            DateTime? minDateQuarterly = null;
+            DateTime? maxDateQuarterly = null;
+
+            DateTime today = DateTime.Now;
+            DateTime march31 = DateTime.Parse($"{today.Year}-03-31");
+            DateTime june30 = DateTime.Parse($"{today.Year}-06-30");
+            DateTime september30 = DateTime.Parse($"{today.Year}-09-30");
+            DateTime december31 = DateTime.Parse($"{today.Year}-12-31");
+
+            // 找出當月最後一天
+            int lastDayInMonth = DateTime.DaysInMonth(today.Year, today.Month);
+
+            // 當月最後一天日期字串
+            string dateTimeMsg = $"{today.Year}-{today.Month}-{lastDayInMonth}";
+
+            maxDateMonthly = DateTime.Parse(dateTimeMsg);
+
+            // 上個月
+            DateTime lastMonth = today.AddMonths(-1);
+
+            // 上個月的第一天
+            dateTimeMsg = $"{lastMonth.Year}-{lastMonth.Month}-1";
+
+            minDateMonthly = DateTime.Parse(dateTimeMsg);
+
+            if (today < march31)
+            {
+                minDateQuarterly = DateTime.Parse($"{today.Year - 1}-7-1");
+                maxDateQuarterly = DateTime.Parse($"{today.Year - 1}-12-31");
+            }
+            else if (today >= march31 && today < june30)
+            {
+                minDateQuarterly = DateTime.Parse($"{today.Year - 1}-10-1");
+                maxDateQuarterly = march31;
+            }
+            else if (today >= june30 && today < september30)
+            {
+                minDateQuarterly = DateTime.Parse($"{today.Year}-1-1");
+                maxDateQuarterly = june30;
+            }
+            else if (today >= september30 && today < december31)
+            {
+                minDateQuarterly = DateTime.Parse($"{today.Year}-4-1");
+                maxDateQuarterly = september30;
+            }
+            else if (today == december31)
+            {
+                minDateQuarterly = DateTime.Parse($"{today.Year}-7-1");
+                maxDateQuarterly = december31;
+            }
+
+            return new List<DateTime>()
+            {
+                minDateMonthly.Value,
+                maxDateMonthly.Value,
+                minDateQuarterly.Value,
+                maxDateQuarterly.Value
+            };
         }
 
         /// <summary>
